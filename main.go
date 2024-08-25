fbf079a99b80a086a37ebce6c303c10b63d74f0d
Uros <uros.tripunovic@omegapoint.se>
2024-08-25 20:47:10 +0200

    Add godoc for the math module

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/tripunovic-uros/learning-go-add/math"
+)
+
+func main() {
+	fmt.Println(math.Add(1,2))
+}
+
--- math/add.go
+++ math/add.go
@@ -1,5 +1,11 @@
+// Package math provides methods for math operations.
 package math
 
+// Add takes in two int paramters and returns the sum
+//
+// More information on addition can be found at [mathisfun]
+// 
+// [mathisfun]: https://mathisfun.com/numbers/addition.html
 func Add(a, b int) int {
   return a + b
 }
