c89b4c41ec3237188d6ba33dbdf8c7e9941cfc89
Uros <uros.tripunovic@omegapoint.se>
2024-08-25 20:59:52 +0200

    Make Add method generic

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/tripunovic-uros/learning-go-add
 
 go 1.22.5
+
+require golang.org/x/exp v0.0.0-20240823005443-9b4947da3948
--- math/add.go
+++ math/add.go
@@ -1,12 +1,19 @@
 // Package math provides methods for math operations.
 package math
 
+import "golang.org/x/exp/constraints"
+
+// A Number is any integer- or float-point type.
+type Number interface {
+  constraints.Integer | constraints.Float
+}
+
 // Add takes in two int paramters and returns the sum
 //
 // More information on addition can be found at [mathisfun]
 // 
 // [mathisfun]: https://mathisfun.com/numbers/addition.html
-func Add(a, b int) int {
+func Add[T Number](a, b T) T {
   return a + b
 }
 
